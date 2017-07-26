840526816ac2cece1c74b5c8b8cbe4a87ae14038
Robb Cunningham <rptc3000@gmail.com>
2017-07-26 12:17:29 -0400

    deleted old templates, removed keystone dep, added UM section, refactored index.html compliation gulp task

---
+++ views/content.js
@@ -0,0 +1,81 @@
+module.exports = {
+  introStatement: 'Believing that culture can propel brands forward in ways that advertising alone can never do, we set out to build a company uniquely suited to today’s world. Yes, we create ads in every shape and form. But we also create full-length films, one of the biggest music festivals in America, TV shows we sell to networks, a line of Air Jordans inspired by a fictitious insurance agent, and far more.',
+
+  sizzleVideoUrl: 'http://www.translationllc.com/assets/films/SIZZLE.mp4',
+  sizzleVideoHeader: 'GREATEST HITS',
+  sizzleVideoContent: 'Whether it’s creating an alter-ego for an NBA All-Star, a music festival for the new America, or giving the king of Snapchat his very own store, our work drives the cultural resonance of our partner brands.',
+
+  workVideoOneUrl: 'http://www.translationllc.com/assets/films/KP.mp4',
+  workVideoOneHeader: 'KP + STEPHEN CURRY RAISE MENTAL HEALTH AWARENESS',
+  workVideoOneContent: 'Quieting the mind to allow the body to thrive, Stephen Curry shows us that the real game begins&nbsp;within.',
+
+  workVideoTwoUrl: 'http://www.translationllc.com/assets/films/NFL.mp4',
+  workVideoTwoHeader: 'A STATEMENT OF UNITY ON THE GAME\'S BIGGEST STAGE',
+  workVideoTwoContent: '“Inside These Lines” shares an uplifting and unifying narrative about players and fans overcoming differences and banding&nbsp;together.',
+
+  workVideoThreeUrl: 'http://www.translationllc.com/assets/films/VCF.mp4',
+  workVideoThreeHeader: 'VCF STAGES A REBRAND WITH A NEW MUSICAL MIX',
+  workVideoThreeContent: 'This commercial-turned-musical stars a woman whose dynamism is reflected by her choice of&nbsp;furniture.',
+
+  workVideoFourUrl: 'http://www.translationllc.com/assets/films/NBA.mp4',
+  workVideoFourHeader: 'THE NBA PLAYOFFS ARE NO JOKE. (FEAT. SHAQ)',
+  workVideoFourContent: 'We wanted to capture this raw and gritty gametime realness. But we couldn\'t do it ourselves. So we hired NBA royalty Shaq to help us out. Because, who else?',
+
+  workVideoFiveUrl: 'http://www.translationllc.com/assets/films/APPLE.mp4',
+  workVideoFiveHeader: 'APPLE MAKES STREAMING MUSIC MORE HUMAN',
+  workVideoFiveContent: 'Whether it’s Kerry Washington talking love & mixtapes or James Corden pitching commercial ideas to Apple, these spots use intimacy and humor to humanize streaming.',
+
+  workVideoSixUrl: 'http://www.translationllc.com/assets/films/BMIA.mp4',
+  workVideoSixHeader: 'BRINGING BUDWEISER BACK WITH "MADE IN AMERICA"',
+  workVideoSixContent: 'At an integral moment in their brand’s history, Budweiser and Jay Z teamed up to bridge the American generational gap using the power of music.',
+
+  workVideoSevenUrl: 'http://www.translationllc.com/assets/films/STATEFARM.mp4',
+  workVideoSevenHeader: 'CHRIS & CLIFF PAUL WERE BORN TO ASSIST',
+  workVideoSevenContent: 'With the birth of Chris Paul’s alter ego, Cliff Paul, came the unlikely union of two disparate worlds: insurance and basketball.',
+
+  workVideoEightUrl: 'http://www.translationllc.com/assets/films/NETS.mp4',
+  workVideoEightHeader: 'NETS UNITE A BOROUGH BY SAYING HELLO',
+  workVideoEightContent: 'Much more than a campaign, “Hello Brooklyn” was the team’s introduction to a borough, and a city.  And our work with the Nets continues today to rally fans and reignite their passion for Brooklyn&nbsp;basketball.',
+
+  workVideoNineUrl: 'http://www.translationllc.com/assets/films/CHAMPSv2.mp4',
+  workVideoNineHeader: 'CHAMPS AND DJ KHALED BRING GAME TO LIFE',
+  workVideoNineContent: 'By building a Champs store inside former employee DJ Khaled’s home, this campaign breathed life into their tagline "We Know Game."',
+
+  articleOnePublication: 'BRAND CHANNEL',
+  articleOneLink: 'http://www.brandchannel.com/2017/06/20/cannes-lions-nba-nfl-062017/',
+  articleOneHeader: 'Cannes 2017: The Wise women of the NBA and NFL',
+  articleOneByline: 'BY: SHEILA SHAYON',
+
+  articleTwoPublication: 'BERKELEY NEWS',
+  articleTwoLink: 'http://news.berkeley.edu/2017/05/19/dj-khaled-surprises-statistics-graduation/',
+  articleTwoHeader: 'Steve Stoute Addresses Berkeley and Surprises Graduates with DJ Khaled',
+  articleTwoByline :'BY: PUBLIC AFFAIRS, UC BERKELEY',
+
+  articleThreePublication: 'CAMPAIGN',
+  articleThreeLink: 'http://www.campaignlive.com/article/career-5-executions-translations-john-norman/1430517',
+  articleThreeHeader: 'My Career in 5 Executions: Translation’s John Norman',
+  articleThreeByline: 'BY: I-HSIEN SHERWOOD',
+
+  articleFourPublication: '',
+  articleFourLink: '',
+  articleFourHeader: '',
+  articleFourByline: '',
+
+  articleFivePublication: 'FAST CO.',
+  articleFourLink: 'https://www.fastcompany.com/40405973/the-nba-playoffs-are-here-and-heres-how-stephen-curry-trains-his-mi',
+  articleFourHeader: 'The NBA Playoffs are Here, and Here’s How Steph Curry Trains His Mind',
+  articleFourByline: 'BY: JEFF BEER',
+
+  articleSixPublication: 'BRAND CHANNEL',
+  articleSixLink: 'http://brandchannel.com/2017/04/10/la-la-land-vcf-041017/',
+  articleSixHeader: 'Tapping La La Land’s Choreographer, VCF is “Made to Mix”',
+  articleSixByline: 'BY: ABE SAUER',
+
+  cultureSectionCopy: 'We believe brands that are brave enough to lead culture will always be more successful than brands chasing to follow it. At Translation, we help ambitious brands thrive in contemporary culture, because we live in it and invite our partners to innovate within it.',
+
+  cultureSectionVideoUrl: 'http://www.translationllc.com/assets/films/BIGGIEBREAKFAST.mp4',
+  cultureSectionVideoHeader: 'A BIG WAY TO SPREAD LOVE IN BROOKLYN',
+  cultureSectionVideoContent: 'To celebrate Biggie’s life and legacy, on March 9, 2016 we took to his hometown of Bed-Stuy, Brooklyn, to feed the needy with a special, signature dish.',
+
+  majorsMinorsContent: 'At Translation, our personal passions are just as important as the titles on our business cards. We are creators, collectors, historians, and more. And in everything we do, we allow our expert fluency across different pockets of culture to spill into the boardroom and elevate the work.'
+}
