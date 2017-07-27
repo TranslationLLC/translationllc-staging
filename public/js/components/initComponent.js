f34eadd61a08b39806dd63a460f4c86934ee0738
Robb Cunningham <rptc3000@gmail.com>
2017-07-27 18:41:02 -0400

    new intro

--- public/js/components/initComponent.js
+++ public/js/components/initComponent.js
@@ -6,11 +6,7 @@ class InitComponent {
     this.__init();
   }
   __init() {
-    this.animationScale = 1;
-    this.animationThreshold = 0;
-    this.endAnimation = false;
     this.initialScroll = true;
-    this.setTransformOnPlus = true;
     window.addEventListener('beforeunload', () => {
       window.scrollTo(0, 0);
     });
@@ -30,9 +26,12 @@ class InitComponent {
     this.mainElement = document.getElementById('translationllcMain');
     this.htmlElement = document.getElementsByTagName('html')[0];
     this.animationElementDesktop = document.getElementById('translationllcAnimationDesktop');
-    this.animationPlusIconDesktop = document.getElementById('animationPlusIconDesktop');
-    this.translationllcWeAreTranslationDesktop = document.getElementById('translationllcWeAreTranslationDesktop');
+    this.animationPlusIconSvg = document.getElementById('animationPlusIconSvg');
     this.animationPlusIcon = document.getElementById('animationPlusIcon');
+    this.animationPlusIcon1 = document.getElementById('animationPlusIcon1');
+    this.animationPlusIcon2 = document.getElementById('animationPlusIcon2');
+    this.translationllcWeAreTranslationDesktop = document.getElementById('translationllcWeAreTranslationDesktop');
+    // this.animationPlusIcon = document.getElementById('animationPlusIcon');
     this.introSectionNav = document.getElementById('introSectionNav');
     this.carouselDesktopWrapper = document.getElementById('carouselDesktopWrapper');
     this.backgroundParallaxOnePlaceholder = document.getElementById('backgroundParallaxOnePlaceholder');
@@ -46,6 +45,39 @@ class InitComponent {
       window.location.hash = '#introSection';
       window.location = window.location.href;
     });
+    this.animationPlusIcon.addEventListener('animationend', () => {
+      this.animationPlusIcon.style.transform = 'scale(50)';
+      this.animationPlusIcon.style['z-index'] = 0;
+      this.animationPlusIcon1.style['z-index'] = 1;
+      this.animationPlusIcon1.style.opacity = 1;
+      this.animationPlusIcon1.classList.add('translationllc__intro__background__plus--desktop--active');
+    });
+    this.animationPlusIcon1.addEventListener('animationend', () => {
+      this.animationPlusIcon.style.opacity = 0;
+      this.animationPlusIcon1.style.transform = 'scale(50)';
+      this.animationPlusIcon1.style['z-index'] = 0;
+      this.animationPlusIcon2.style['z-index'] = 1;
+      this.animationPlusIcon2.style.opacity = 1;
+      this.animationPlusIcon2.classList.add('translationllc__intro__background__plus--desktop--active');
+    });
+    this.animationPlusIcon2.addEventListener('animationend', () => {
+      this.animationPlusIcon1.style.opacity = 0;
+      this.animationPlusIcon2.style.opacity = 0;
+      this.introElement.style.display = 'none';
+      this.mainElement.style.display = 'block';
+      if (this.isSafari) {
+        this.htmlElement.style.overflow = null;
+      }
+      this.body.style.touchAction = 'auto';
+      this.navElement.style.opacity = 1;
+      this.navElement.style.top = 0;
+      this.mainElement.style.opacity = 1;
+      if (window.location.hash) {
+        window.scrollTo(0, document.getElementById(window.location.hash.split('#')[1]).offsetTop);
+      } else {
+        window.scrollTo(0, 0);
+      }
+    });
   }
   __currentPosition() {
     return window.pageYOffset;
@@ -53,7 +85,7 @@ class InitComponent {
   __showIntro(detectionData) {
     if (detectionData.isDesktop) {
       this.mainElement.style.display = 'none';
-      this.animationPlusIcon.style.display = 'none';
+      // this.animationPlusIcon.style.display = 'none';
       this.translationllcWeAreTranslation.style.display = 'none';
       this.animationScrollHandlerDesktop = this.__animationScrollHandler.bind(this);
       this.eventBus.addChangeListener('signalScroll', this.animationScrollHandlerDesktop);
@@ -68,21 +100,21 @@ class InitComponent {
   __resizeSvgs(detectionData) {
     if (detectionData.isDesktop || (detectionData.isIpad && Math.abs(detectionData.orientation) === 90)) {
       this.translationllcWeAreTranslationDesktop.classList.remove('hide-component');
-      this.animationPlusIconDesktop.classList.remove('hide-component');
-      if (detectionData.isDesktop) {
-        this.carouselDesktopWrapper.classList.remove('hide-component');
-      }
-      this.animationPlusIconDesktop.style.backgroundSize = `${this.animationPlusIconDesktop.getBoundingClientRect().height / 2}px`;
+      // this.animationPlusIcon.classList.remove('hide-component');
+      // if (detectionData.isDesktop) {
+      //   this.carouselDesktopWrapper.classList.remove('hide-component');
+      // }
+      // this.animationPlusIcon.style.backgroundSize = `${this.animationPlusIcon.getBoundingClientRect().height / 2}px`;
     } else if (detectionData.orientation === 0) {
       //77 when logo is in the header
-      this.animationPlusIcon.style.height = `${Math.floor(Math.ceil(window.innerHeight / 2 + 100 - 44) * .6)}px`;
-      this.animationPlusIcon.style.width = '50%';
+      // this.animationPlusIcon.style.height = `${Math.floor(Math.ceil(window.innerHeight / 2 + 100 - 44) * .6)}px`;
+      // this.animationPlusIcon.style.width = '50%';
       this.translationllcWeAreTranslation.style.height = `${Math.floor(Math.ceil(window.innerHeight / 2 + 100 - 44) * .6)}px`;
       this.translationllcWeAreTranslation.style.top = '50%';
       this.translationllcWeAreTranslation.transform = 'translateY(-50%)';
     } else if (Math.abs(detectionData.orientation) === 90) {
-      this.animationPlusIcon.style.height = '100%';
-      this.animationPlusIcon.style.width = '40%';
+      // this.animationPlusIcon.style.height = '100%';
+      // this.animationPlusIcon.style.width = '40%';
       this.translationllcWeAreTranslation.style.height = '50%';
       this.translationllcWeAreTranslation.style.top = '40%';
     }
@@ -102,30 +134,21 @@ class InitComponent {
   }
   __animationScrollHandler(data) {
     if (!this.initialScroll) {
-      this.animationThreshold = 20;
-      if (!this.isDesktop) {
-        this.animationThreshold = 16;
-        this.animationScale = this.animationScale + 2;
-      } else if (this.isSafari) {
-        this.animationThreshold = 40;
-        this.animationScale = this.animationScale + 5;
-      } else {
-        this.animationScale = this.animationScale + 3;
-      }
-      this.animationPlusIconDesktop.style.top = '50%';
-      this.animationPlusIconDesktop.classList.add('translationllc__intro__background__plus--desktop--active');
+      WindowActions.endAnimation();
       this.animationPlusIcon.style.top = '50%';
       this.animationPlusIcon.classList.add('translationllc__intro__background__plus--desktop--active');
-      window.setTimeout(() => {
-        this.introElement.style.display = 'none';
-        this.mainElement.style.display = 'block';
-        let lastHash = false;
-        if (window.location.hash) {
-          window.scrollTo(0, document.getElementById(window.location.hash.split('#')[1]).offsetTop);
-        } else {
-          window.scrollTo(0, 0);
-        }
-      }, 800);
+      // this.animationPlusIcon.style.top = '50%';
+      this.animationPlusIcon.classList.add('translationllc__intro__background__plus--desktop--active');
+      this.animationPlusIconSvg.classList.add('translationllc__intro__background__plus--svg--active')
+      // window.setTimeout(() => {
+      //   this.introElement.style.display = 'none';
+      //   this.mainElement.style.display = 'block';
+      //   if (window.location.hash) {
+      //     window.scrollTo(0, document.getElementById(window.location.hash.split('#')[1]).offsetTop);
+      //   } else {
+      //     window.scrollTo(0, 0);
+      //   }
+      // }, 1000);
       if (this.animationScrollHandlerDesktop) {
         this.eventBus.removeChangeListener('signalScroll', this.animationScrollHandlerDesktop);
       }
@@ -133,18 +156,17 @@ class InitComponent {
         this.eventBus.removeChangeListener('keydown', this.handleKeydownHandler);
       }
       this.eventBus.removeChangeListener('signalTouchmove', this.animationScrollHandler);
-      WindowActions.endAnimation();
-      window.setTimeout(() => {
-        if (this.isSafari) {
-          this.htmlElement.style.overflow = null;
-        }
-      }, 500);
-      window.setTimeout(() => {
-        this.body.style.touchAction = 'auto';
-        this.navElement.style.opacity = 1;
-        this.navElement.style.top = 0;
-        this.mainElement.style.opacity = 1;
-      }, 500);
+      // window.setTimeout(() => {
+      //   if (this.isSafari) {
+      //     this.htmlElement.style.overflow = null;
+      //   }
+      // }, 500);
+      // window.setTimeout(() => {
+      //   this.body.style.touchAction = 'auto';
+      //   this.navElement.style.opacity = 1;
+      //   this.navElement.style.top = 0;
+      //   this.mainElement.style.opacity = 1;
+      // }, 500);
     }
     this.initialScroll = false;
   }
